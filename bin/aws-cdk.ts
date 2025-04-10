6a8c7a5c3419402d1f1d2043a90f2449623423d2
fqazi-gencloud <faisal.qazi@genclouds.com>
2025-04-10 19:27:33 +0500

    new changes

--- bin/aws-cdk.ts
+++ bin/aws-cdk.ts
@@ -5,13 +5,26 @@ import { VpcStack } from '../lib/vpc-stack';
 import { DatabaseStack } from '../lib/database-stack';
 import { EcrStack } from '../lib/ecr-stack';
 import { AppRunnerStack } from '../lib/apprunner-stack';
+import { ElasticacheStack } from '../lib/elasticache-stack';
 
 const app = new cdk.App();
 
+// Existing stacks
 const vpcStack = new VpcStack(app, 'VpcStack');
-new DatabaseStack(app, 'DatabaseStack', { vpc: vpcStack.vpc });
+const databaseStack = new DatabaseStack(app, 'DatabaseStack', { vpc: vpcStack.vpc });
 const ecrStack = new EcrStack(app, 'EcrStack');
-new AppRunnerStack(app, 'AppRunnerStack', {
+const appRunnerStack = new AppRunnerStack(app, 'AppRunnerStack', {
   nodejsRepo: ecrStack.nodejsRepo,
   fastapiRepo: ecrStack.fastapiRepo,
 });
+
+// Add ElastiCache stack
+new ElasticacheStack(app, 'ElasticacheStack', {
+  vpc: vpcStack.vpc,
+  appRunnerRole: appRunnerStack.instanceRole,
+});
+
+// Add dependencies
+databaseStack.addDependency(vpcStack);
+appRunnerStack.addDependency(ecrStack);
+
