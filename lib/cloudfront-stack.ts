fd7a2da0bfad9a9708675828eb29460bd444fe1c
fqazi-genclouds <faisal.qazi@genclouds.com>
2025-07-21 19:42:56 +0500

    Adding cloudfront stack for s3 bucket creation & origin set

--- lib/cloudfront-stack.ts
+++ lib/cloudfront-stack.ts
@@ -5,15 +5,17 @@ import * as ssm from 'aws-cdk-lib/aws-ssm';
 import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as route53 from 'aws-cdk-lib/aws-route53';
 import * as route53Targets from 'aws-cdk-lib/aws-route53-targets';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 interface CloudFrontStackProps extends cdk.StackProps {
-  nodejsServiceUrl: string;
-  fastapiServiceUrl: string;
+  // No dependencies needed
 }
 
 export class CloudFrontStack extends cdk.Stack {
   public readonly distribution: cloudfront.Distribution;
+  public readonly bucket: s3.Bucket;
 
   constructor(scope: Construct, id: string, props: CloudFrontStackProps) {
     super(scope, id, props);
@@ -25,6 +27,29 @@ export class CloudFrontStack extends cdk.Stack {
     // Define domain name based on environment
     const domainName = `${envPrefix}app.yahyagulshan.com`;
     
+    // Create S3 bucket for static web hosting
+    this.bucket = new s3.Bucket(this, 'WebsiteBucket', {
+      bucketName: domainName,
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html',
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+    
+    // Create Origin Access Identity for CloudFront
+    const originAccessIdentity = new cloudfront.OriginAccessIdentity(this, 'OriginAccessIdentity', {
+      comment: `OAI for ${domainName}`,
+    });
+    
+    // Grant read permissions to CloudFront
+    this.bucket.addToResourcePolicy(new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [this.bucket.arnForObjects('*')],
+      principals: [new iam.CanonicalUserPrincipal(originAccessIdentity.cloudFrontOriginAccessIdentityS3CanonicalUserId)],
+    }));
+    
     // Use the provided wildcard certificate for yahyagulshan.com
     const certificateArn = 'arn:aws:acm:us-east-1:019721216237:certificate/a09c161c-72db-473b-9192-1f56b87ea531';
     const certificate = acm.Certificate.fromCertificateArn(this, 'Certificate', certificateArn);
@@ -32,21 +57,25 @@ export class CloudFrontStack extends cdk.Stack {
     // Create CloudFront distribution
     this.distribution = new cloudfront.Distribution(this, 'Distribution', {
       defaultBehavior: {
-        origin: new origins.HttpOrigin(props.nodejsServiceUrl.replace('https://', '')),
-        allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
-        cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
-        originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER,
+        origin: new origins.S3Origin(this.bucket, { originAccessIdentity }),
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       },
-      additionalBehaviors: {
-        '/api/*': {
-          origin: new origins.HttpOrigin(props.fastapiServiceUrl.replace('https://', '')),
-          allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
-          cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
-          originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER,
-          viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-        }
-      },
+      errorResponses: [
+        {
+          httpStatus: 403,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+        },
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+        },
+      ],
+      defaultRootObject: 'index.html',
+      // No additional behaviors needed
       domainNames: [domainName],
       certificate: certificate,
       comment: `${environment} environment distribution`,
@@ -71,6 +100,18 @@ export class CloudFrontStack extends cdk.Stack {
       value: domainName,
       description: 'The custom domain name of the CloudFront distribution',
       exportName: environment === 'dev' ? 'CloudFrontCustomDomainDev' : 'CloudFrontCustomDomain',
+    });
+    
+    new cdk.CfnOutput(this, 'S3BucketName', {
+      value: this.bucket.bucketName,
+      description: 'The name of the S3 bucket for static website hosting',
+      exportName: environment === 'dev' ? 'S3BucketNameDev' : 'S3BucketName',
+    });
+    
+    new cdk.CfnOutput(this, 'S3BucketArn', {
+      value: this.bucket.bucketArn,
+      description: 'The ARN of the S3 bucket for static website hosting',
+      exportName: environment === 'dev' ? 'S3BucketArnDev' : 'S3BucketArn',
     });
     
     // Create Route53 record with the provided hosted zone ID
